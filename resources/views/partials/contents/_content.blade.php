3265b494afe6a535dac04bc8e39c585513268819
Gnilane95 <agathegnilanediouf@gmail.com>
2022-09-22 14:03:46 +0200

    Content left ok

---
+++ resources/views/partials/contents/_content.blade.php
@@ -0,0 +1,29 @@
+<div class="flex gap-4 mx-72 my-5">
+    <div class="w-2/3 ">
+        <div class="mb-5 bg-white">
+            <x-card h3="Title Heading" h4="Title description" date="May 2, 2016" url_img="girl_hat.jpeg" titledesc="More Hats !" description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde in amet mollitia impedit officiis libero magni ex iure commodi distinctio tempore eligendi corporis et dignissimos quam laborum placeat repellat facilis, alias autem optio fuga exercitationem sed voluptatem. Quos, similique ea!" paragraph="Unde in amet mollitia impedit officiis libero magni ex iure commodi distinctio tempore eligendi corporis et dignissimos quam laborum placeat repellat facilis, alias autem optio fuga exercitationem sed voluptatem." repliesNum="1">
+                <hr class="mt-14">
+                <div class="mt-7 flex space-x-24 align-items-center">
+                    <img src="img/avatar_smoke.jpeg" alt="picture followers" class="w-24">
+                    <div class="pt-3">
+                        <p><span class="text-2xl font-bold">Georges</span> <span class="text-gray-500">May 3, 2015, 6:32 PM</span></p>
+                        <p class="pt-3">Great blog post ! Following</p>
+                    </div>
+                </div>
+            </x-card> 
+        </div>    
+        <div class="mb-5 bg-white">
+            <x-card h3="Title Heading" h4="Title description" date="April 23, 2016" url_img="man_hat.jpeg" titledesc="Hats !" description="Lorem ipsum dolor sit amet consectetur adipisicing elit." paragraph="Unde in amet mollitia impedit officiis libero magni ex iure commodi distinctio tempore eligendi corporis et dignissimos quam laborum placeat repellat facilis, alias autem optio fuga exercitationem sed voluptatem. Unde in amet mollitia impedit officiis libero magni ex iure commodi distinctio tempore eligendi corporis et dignissimos quam laborum placeat repellat facilis, alias autem optio fuga exercitationem sed voluptatem. Quos, similique ea!" repliesNum="2">
+            </x-card> 
+        </div>    
+        <div class="bg-white">
+            <x-card h3="Title Heading" h4="Title description" date="April 7, 2016" url_img="runway.jpeg" titledesc="Don't miss !" description="Lorem ipsum dolor sit amet consectetur adipisicing elit." paragraph="Unde in amet mollitia impedit officiis libero magni ex iure commodi distinctio tempore eligendi corporis et dignissimos quam laborum placeat repellat facilis, alias autem optio fuga exercitationem sed voluptatem.Unde in amet mollitia impedit officiis libero magni ex iure commodi distinctio tempore eligendi corporis et dignissimos quam laborum placeat repellat facilis, alias autem optio fuga exercitationem sed voluptatem." repliesNum="3">
+            </x-card> 
+        </div>    
+    </div>
+    <div class="w-1/3 bg-white">
+        <div>
+            <x-section1Right/> 
+        </div>
+    </div>
+</div>
